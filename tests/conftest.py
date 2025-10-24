75d527b4bbc89c0c2adf89d5b5f1bcfe1b105d47
Zach Sailer <zsailer@apple.com>
2025-10-23 20:30:52 -0700

    add unit tests and workflows

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+"""Pytest configuration and fixtures for nextgen-kernels-api tests."""
+
+import pytest
+
+# Import pytest-jupyter fixtures for Jupyter Server
+pytest_plugins = ["pytest_jupyter.jupyter_server"]
